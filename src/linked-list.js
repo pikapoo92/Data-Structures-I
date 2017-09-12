1dba1ed87eb785d79ff484eb07dd31439252b348
Keagan Goetsch <misterrorypond@gmail.com>
2017-09-11 19:26:44 -0500

    data structures 1 homework sans EC

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,38 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+
+  addToTail(value) {
+      const newNode = {
+      value,
+      next: null,
+    };
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+
+  removeHead() {
+    if (this.head) {
+      const value = this.head.value;
+      this.head = this.head.next;
+      return value;
+    }
+  }
+
+  contains(val) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === val) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,19 @@
 class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  add(value) {
+    return this.storage.push(value);
+  }
+
+  remove() {
+    return this.storage.pop();
+  }
+
+  get size() {
+    return this.storage.length;
+  }
 
 }
 
